073c6124df990bc407103bd6376bcec06714a406
Dominic Kgoete <49305704+lorthbroth@users.noreply.github.com>
2020-03-18 08:04:47 +0200

    Topic 7

--- Topic_7/src/com/company/Main.java
+++ Topic_7/src/com/company/Main.java
@@ -19,9 +19,13 @@ public class Main {
         Awesome2 dominique = new Awesome2();
 
         dominic.whoAreYou();
-        //dominique.whoAreYou(); //this method can access the same method as the line above.
+        dominique.whoAreYou(); //this method can access the same method as the line above.
         //we can access same methods through the extense
         dominique.whereYoufrom();
+
+        usingSuperAndThis usingSuperAndThis = new usingSuperAndThis(0);
+        usingSuperAndThis.addNumbrerPlusOne(2);
+
 
         System.out.println("\n");
 
@@ -33,12 +37,10 @@ public class Main {
         };
 
         for (int i = 0; i < teams.length; i++){
-            teams[i].wins();//with this method you can access all the classes through the interface.
-            //teams[i].scores();
+            //teams[i].wins();//with this method you can access all the classes through the interface.
+            teams[i].scores();
             //teams[i].rank();
         }
 
-        usingSuperAndThis usingSuperAndThis = new usingSuperAndThis(0);
-        usingSuperAndThis.addNumbrerPlusOne(2);
     }
 }
