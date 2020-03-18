e1369bbffc9b0bf57ae3d66bbb9e3b61dc99046d
Dominic Kgoete <49305704+lorthbroth@users.noreply.github.com>
2020-03-18 08:08:16 +0200

    Topic_8
    Topic 8 : Handling Exceptions
    Objectives  :
    
    Differentiate among checked exceptions, unchecked exceptions, and Errors
    Create a try-catch block and determine how exceptions alter normal program flow
    Describe the advantages of Exception handling
    Create and invoke a method that throws an exception
    Recognize common exception classes (such as NullPointerException, ArithmeticException, ArrayIndexOutOfBoundsException, ClassCastException)

---
+++ Topic_8/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+/*
+* Topic 8 : Handling Exceptions
+*Objectives  :
+*
+*1. Differentiate among checked exceptions, unchecked exceptions, and Errors
+*2. Create a try-catch block and determine how exceptions alter normal program flow
+*3. Describe the advantages of Exception handling
+*4. Create and invoke a method that throws an exception
+*5. Recognize common exception classes
+* (such as NullPointerException, ArithmeticException, ArrayIndexOutOfBoundsException, ClassCastException)
+* */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
